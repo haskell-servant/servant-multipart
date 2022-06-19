665c20d5977b391f6b15f826a941c7b7f377b08b
Jan Hrcek <2716069+jhrcek@users.noreply.github.com>
2022-06-19 04:22:27 +0200

    Haddock corrections (#62)
    * Fix typos in haddocks
    
    * Use correct function name

--- servant-multipart-api/src/Servant/Multipart/API.hs
+++ servant-multipart-api/src/Servant/Multipart/API.hs
@@ -110,13 +110,13 @@ import qualified Data.ByteString.Lazy as LBS
 --   @
 --
 --   Note that the behavior of this combinator is configurable,
---   by using 'serveWith' from servant-server instead of 'serve',
+--   by using 'serveWithContext' from servant-server instead of 'serve',
 --   which takes an additional 'Context' argument. It simply is an
 --   heterogeneous list where you can for example store
 --   a value of type 'MultipartOptions' that has the configuration that
 --   you want, which would then get picked up by servant-multipart.
 --
---   __Important__: as mentionned in the example above,
+--   __Important__: as mentioned in the example above,
 --   the file paths point to temporary files which get removed
 --   after your handler has run, if they are still there. It is
 --   therefore recommended to move or copy them somewhere in your
@@ -208,7 +208,7 @@ instance FromMultipart tag (MultipartData tag) where
 --   @
 --   data User = User { username :: Text, pic :: FilePath }
 --
---   instance toMultipart Tmp User where
+--   instance ToMultipart Tmp User where
 --       toMultipart user = MultipartData [Input "username" $ username user]
 --                                        [FileData "pic"
 --                                                  (pic user)
