a2bf235bd5ade12226088a0c8cd169ba65045dfe
Giorgio Marinelli <giorgio@marinel.li>
2023-09-03 17:32:08 +0200

    Remove base-compat and use TypeApplications

--- servant-multipart-client/src/Servant/Multipart/Client.hs
+++ servant-multipart-client/src/Servant/Multipart/Client.hs
@@ -27,7 +27,6 @@ import Servant.Multipart.API
 import Control.Monad (replicateM)
 import Data.Array (listArray, (!))
 import Data.List (foldl')
-import Data.List.Compat (singleton)
 #if !MIN_VERSION_base(4,11,0)
 import Data.Monoid ((<>))
 #endif
@@ -75,7 +74,7 @@ instance MultipartClient Tmp where
         readHandle hdl = fromActionStep LBS.null (LBS.hGet hdl 4096)
 
 instance MultipartClient Mem where
-    loadFile _ = source . singleton
+    loadFile _ = source . pure @[]
 
 -- | Generates a boundary to be used to separate parts of the multipart.
 -- Requires 'IO' because it is randomized.
@@ -133,7 +132,7 @@ multipartToBody boundary mp = RequestBodySource $ files' <> source ["--", bounda
     renderInput input = renderPart (lencode . iName $ input)
                                    "text/plain"
                                    ""
-                                   (source . singleton . lencode . iValue $ input)
+                                   (source . pure @[] . lencode . iValue $ input)
     inputs' = foldl' (\acc x -> acc `mappend'` renderInput x) mempty' (inputs mp)
     renderFile :: FileData tag -> SourceIO LBS.ByteString
     renderFile file = renderPart (lencode . fdInputName $ file)
